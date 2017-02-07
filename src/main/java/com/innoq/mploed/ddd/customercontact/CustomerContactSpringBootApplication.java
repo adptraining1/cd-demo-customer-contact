bc4ae55be2163cf9f95a28f7a06cc0aed388056c
mploed <michael.ploed@gmail.com>
2017-02-07 08:04:19 +0100

    initial commit

---
+++ src/main/java/com/innoq/mploed/ddd/customercontact/CustomerContactSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.innoq.mploed.ddd.customercontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerContactSpringBootApplication {
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(CustomerContactSpringBootApplication.class, args);
+    }
+}
