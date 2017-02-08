413d36dcf169b8fbb86b6b5eabe1ade5faab9171
mploed <michael.ploed@gmail.com>
2017-02-08 08:10:12 +0100

    Added logback logging to json

--- src/main/java/com/innoq/mploed/ddd/customercontact/receiver/CreditApplicationApprovedEventReceiver.java
+++ src/main/java/com/innoq/mploed/ddd/customercontact/receiver/CreditApplicationApprovedEventReceiver.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import static net.logstash.logback.argument.StructuredArguments.*;
+
 
 @Component
 public class  CreditApplicationApprovedEventReceiver {
@@ -19,7 +21,7 @@ public class  CreditApplicationApprovedEventReceiver {
     }
 
     public void receiveMessage(CreditApplicationApprovedEvent creditApplicationApprovedEvent) {
-        LOGGER.info("Received a CreditApplicationApprovedEvent for further processing: " + creditApplicationApprovedEvent.toString());
+        LOGGER.info("Received a CreditApplicationApprovedEvent for further processing: " + creditApplicationApprovedEvent.toString(), value("business_event", "CreditApplicationApprovedEvent"), value("type", "incoming_event"));
         Address address = addressRepository.findByCustomerId(creditApplicationApprovedEvent.getCustomerId());
         LOGGER.info("Sending a new credit contract to the following Address: " + address);
     }
